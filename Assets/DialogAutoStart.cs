58ed29f5ef61516fe8279061b1db61042557bfca
Galilee Mason <mason.galilee@gmail.com>
2025-12-11 01:34:26 +0100

    Add dialogs, icons, update and fix stuff
    conflict for sureeeee

--- Assets/DialogAutoStart.cs
+++ Assets/DialogAutoStart.cs
@@ -5,12 +5,34 @@ public class DialogAutoStart : MonoBehaviour
     [SerializeField]
     private DialogAsset dialogToStart;
 
+    [SerializeField]
+    private DialogAsset AlternativeDialog;
+
+    [SerializeField]
+    private GameObject previousPOV;
+
+    [SerializeField]
+    private CollectibleItem itemToCheck;
+
+    [SerializeField]
+    private AnimatedSprite interlocutorSprite;
+
     private void Start()
     {
         DialogManager dialogManager = FindFirstObjectByType<DialogManager>();
-        if (dialogManager != null && dialogToStart != null)
+        if (dialogManager != null && dialogToStart != null && AlternativeDialog != null)
         {
-            dialogManager.StartDialog(dialogToStart);
+            dialogManager.SetInterlocutorSprite(interlocutorSprite);
+            dialogManager.SetCharonVoice();
+            dialogManager.SetPreviousPOV(previousPOV);
+            if (itemToCheck != null && GameManager.Instance.IsItemPentacled(itemToCheck))
+            {
+                dialogManager.StartDialog(AlternativeDialog);
+            }
+            else
+            {
+                dialogManager.StartDialog(dialogToStart);
+            }
         }
     }
 }
--- Assets/Scripts/ItemSpot.cs
+++ Assets/Scripts/ItemSpot.cs
@@ -30,7 +30,6 @@ public class ItemSpot : MonoBehaviour, IDropHandler
     {
         if (transform.childCount == 0)
         {
-            Debug.Log("returning null");
             return null;
         }
 
